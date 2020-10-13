375cfdd136d583e39bfeeefd2d22881e19b31cbc
allicn03 <a.neusch@gmail.com>
2020-10-13 01:32:15 -0400

    4.3 create orders controller and views, change homepage to landing_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
-
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
+  # root 'simple_pages#index'
   get 'simple_pages/index'
 
 
