f2b309c6c588b7f9a871f52efdc63514dcba6d0c
allicn03 <a.neusch@gmail.com>
2020-10-30 13:16:37 -0400

    5.2 add search form to nav and add postgres functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    def self.search (search_term)
+        if Rails.env.production?
+            Product.where("name ilike ? OR description ilike ?", "%#{search_term}%", "%#{search_term}%")
+        else
+            Product.where("name LIKE ? OR description LIKE ?", "%#{search_term}%", "%#{search_term}%")
+        end
+    end
 end
