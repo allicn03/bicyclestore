0f97784643b023f0ba0fc45f749b248a342db02d
allicn03 <a.neusch@gmail.com>
2020-10-31 14:49:08 -0400

    set up email in development with generic addresses

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # root 'simple_pages#index'
   get 'simple_pages/index'
 
+  post 'simple_pages/thank_you'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
